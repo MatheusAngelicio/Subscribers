b9eda578af86b6d785b825da9b16cb7fc0262339
Matheus Angelicio <matheus.angelicio00@gmail.com>
2021-10-16 18:59:29 -0300

    Initial commit

---
+++ app/src/main/java/br/tecpuc/subscribers/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.tecpuc.subscribers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
