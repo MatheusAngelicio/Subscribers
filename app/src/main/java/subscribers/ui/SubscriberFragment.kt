4923c52e8a4b8a240bf5d8fe39d676dbd9ddd88e
Matheus Angelicio <matheus.angelicio00@gmail.com>
2021-10-18 15:47:59 -0300

    Fazer insercao do subscriber

--- app/src/main/java/subscribers/ui/SubscriberFragment.kt
+++ app/src/main/java/subscribers/ui/SubscriberFragment.kt
@@ -4,14 +4,34 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import br.tecpuc.subscribers.R
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.subscriber_fragment.*
 import subscribers.data.db.AppDatabase
-import subscribers.data.db.entity.SubscriberEntity
+import subscribers.data.db.dao.SubscriberDAO
+import subscribers.extensions.hideKeyboard
+import subscribers.repository.DatabaseDataSource
+import subscribers.repository.SubscriberRepository
 
 class SubscriberFragment : Fragment() {
 
-    private lateinit var viewModel: SubscriberViewModel
+    private val viewModel: SubscriberViewModel by viewModels {
+        object : ViewModelProvider.Factory {
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                val subscriberDAO: SubscriberDAO =
+                    AppDatabase.getInstance(requireContext()).subscriberDAO
+
+                val repository: SubscriberRepository = DatabaseDataSource(subscriberDAO)
+                return SubscriberViewModel(repository) as T
+            }
+
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,5 +39,51 @@ class SubscriberFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.subscriber_fragment, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        observeViewModel()
+        setListeners()
+    }
+
+
+
+    private fun observeViewModel() {
+        viewModel.subscriberStateEventData.observe(viewLifecycleOwner) { subscriberState ->
+            when(subscriberState) {
+                is SubscriberViewModel.SubscriberState.Inserted -> {
+                    clearFields()
+                    hideKeyboard()
+                }
+            }
+        }
+
+        viewModel.messageEventData.observe(viewLifecycleOwner) { stringResId ->
+            Snackbar.make(requireView(), stringResId, Snackbar.LENGTH_LONG).show()
+        }
+    }
+
+    private fun clearFields() {
+        input_name.text?.clear()
+        input_email.text?.clear()
+    }
+
+    private fun hideKeyboard() {
+        val parentActivity = requireActivity()
+        if (parentActivity is AppCompatActivity) {
+            parentActivity.hideKeyboard()
+        }
+    }
+
+    private fun setListeners() {
+        button_subscriber.setOnClickListener {
+            val name = input_name.text.toString()
+            val email = input_email.text.toString()
+
+            viewModel.addSubscriber(name,email)
+        }
+    }
+
 
 }
